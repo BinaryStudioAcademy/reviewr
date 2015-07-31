c0f07be744b1050c8e4dc921e21aa96454cc0926
al0mie <alex.mokrencko@yandex.ru>
2015-07-31 19:42:28 +0300

    add auth routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
