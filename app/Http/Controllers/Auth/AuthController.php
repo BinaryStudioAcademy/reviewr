d76e8eb208d1fea1a2b5463ceb55bdaa8047e1de
al0mie <al0mie@users.noreply.github.com>
2015-08-04 11:05:57 +0300

    Update AuthController.php

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
 
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
-    protected $redirectPath = '/application';
+    protected $redirectPath = '/';
 
     /**
      * Create a new authentication controller instance.
