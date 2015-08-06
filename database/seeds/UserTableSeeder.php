d182520f39f218dc1cc250aa414dfa95aeef0752
loginreegerye <reegerye@gmail.com>
2015-08-06 17:00:03 +0300

    added test users

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -15,6 +15,32 @@ class UserTableSeeder extends Seeder
         $jobIds = Job::lists('id')->toArray();
         $departmentIds = Department::lists('id')->toArray();
 
+        User::create([
+                'first_name' => 'admin',
+                'last_name' => 'admin',
+                'email' => 'admin@email.com',
+                'phone' => $faker->phoneNumber,
+                'avatar' => $faker->imageUrl(150, 150, 'abstract'),
+                'address' => $faker->address,
+                'password' => bcrypt('password'),
+                'reputation' => $faker->randomDigitNotNull,
+                'job_id' => $faker->randomElement($jobIds),
+                'department_id' => $faker->randomElement($departmentIds)
+            ]);
+
+        User::create([
+                'first_name' => 'test',
+                'last_name' => 'test',
+                'email' => 'test@email.com',
+                'phone' => $faker->phoneNumber,
+                'avatar' => $faker->imageUrl(150, 150, 'abstract'),
+                'address' => $faker->address,
+                'password' => bcrypt('password'),
+                'reputation' => $faker->randomDigitNotNull,
+                'job_id' => $faker->randomElement($jobIds),
+                'department_id' => $faker->randomElement($departmentIds)
+            ]);
+
         foreach(range(1,20) as $index)
         {
             User::create([
