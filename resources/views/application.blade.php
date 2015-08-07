0a29a3aa46601dfc02dca462be2e3737fcde0b01
loginreegerye <reegerye@gmail.com>
2015-08-07 15:42:19 +0300

    template login fix

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -152,7 +152,7 @@
                 <div class="panel panel-default">
                     <div class="panel-heading">CREATE REQUEST</div>
                     <div class="panel-body">
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('#') }}">
+                        <form class="form-horizontal" role="form" method="POST" action="{{ URL::to('reviewr/auth/logout') }}">
 
                             <div class="form-group">
                                 <label class="col-md-4 control-label">Title</label>
