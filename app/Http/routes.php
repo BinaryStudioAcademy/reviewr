3e35d5da7dc0bef8e996a4f33fd7340bdc3bbec3
a1ex7 <artstroy14@mail.ru>
2015-08-12 20:58:55 +0300

    lost brakets

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,9 +1,14 @@
 <?php
 
 Route::group (['prefix' => ''], function () {
-    Route::get('/', ['as' => 'home', 'middleware' => 'auth', function() {
-        return view('application');
-    }]);
+
+    Route::get('/', [
+        'as'         => 'home',
+        'middleware' => 'auth',
+        function () {
+            return view('application');
+        }
+    ]);
 
     Route::get('/auth/login', [
         'as'   => 'login.get',
@@ -18,20 +23,21 @@ Route::group (['prefix' => ''], function () {
         'uses' => 'Auth\AuthController@getLogout'
     ]);
 
-Route::group (['prefix' => 'api/v1'], function () {
-    Route::get('reviewrequest/my', 'ReviewRequestController@myReviewRequest');
-    Route::resource('user', 'UserController');
-    Route::resource('comment', 'CommentController');
-    Route::resource('job', 'JobController');
-    Route::resource('department', 'DepartmentController');
-    Route::resource('reviewrequest', 'ReviewRequestController');
-    Route::get('reviewrequest/{id}/offers', 'ReviewRequestController@offers');
-    Route::get('reviewrequest/{id}/tags', 'ReviewRequestController@tags');
-    Route::get('user/{user_id}/accept/{request_id}', 'UserController@acceptReviewRequest');
-    Route::get('user/{user_id}/decline/{request_id}', 'UserController@declineReviewRequest');
-    Route::get('user/{user_id}/offeron/{request_id}', 'UserController@offerOnReviewRequest');
-    Route::resource('group', 'GroupController');
-    Route::resource('tag', 'TagController');
-    Route::resource('badge', 'BadgeController');
-    Route::post('tags/search', "TagController@search");
+    Route::group([ 'prefix' => 'api/v1' ], function () {
+        Route::get('reviewrequest/my', 'ReviewRequestController@myReviewRequest');
+        Route::resource('user', 'UserController');
+        Route::resource('comment', 'CommentController');
+        Route::resource('job', 'JobController');
+        Route::resource('department', 'DepartmentController');
+        Route::resource('reviewrequest', 'ReviewRequestController');
+        Route::get('reviewrequest/{id}/offers', 'ReviewRequestController@offers');
+        Route::get('reviewrequest/{id}/tags', 'ReviewRequestController@tags');
+        Route::get('user/{user_id}/accept/{request_id}', 'UserController@acceptReviewRequest');
+        Route::get('user/{user_id}/decline/{request_id}', 'UserController@declineReviewRequest');
+        Route::get('user/{user_id}/offeron/{request_id}', 'UserController@offerOnReviewRequest');
+        Route::resource('group', 'GroupController');
+        Route::resource('tag', 'TagController');
+        Route::resource('badge', 'BadgeController');
+        Route::post('tags/search', "TagController@search");
+    });
 });
