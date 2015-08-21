7ddf546b9eefeaf05e9e4ac71f31c76120c5b0b3
al0mie <alex.mokrencko@yandex.ru>
2015-08-21 12:29:10 +0300

    seed real user

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -41,6 +41,73 @@ class UserTableSeeder extends Seeder
                 'department_id' => $faker->randomElement($departmentIds)
             ]);
 
+        User::create([
+                'first_name' => 'Alexey',
+                'last_name' => 'Tsinya',
+                'email' => 'tsinya.alexey@gmail.com',
+                'phone' => $faker->phoneNumber,
+                'avatar' => 'http://www.gravatar.com/avatar/' . $faker->md5. '?d=identicon',
+                'address' => $faker->address,
+                'password' => bcrypt('password'),
+                'reputation' => $faker->randomDigitNotNull,
+                'job_id' => $faker->randomElement($jobIds),
+                'department_id' => $faker->randomElement($departmentIds)
+            ]);
+
+
+        User::create([
+                'first_name' => 'Alex',
+                'last_name' => 'Mokrenko',
+                'email' => 'alex.mokrencko@yandex.ru',
+                'phone' => $faker->phoneNumber,
+                'avatar' => 'http://www.gravatar.com/avatar/' . $faker->md5. '?d=identicon',
+                'address' => $faker->address,
+                'password' => bcrypt('password'),
+                'reputation' => $faker->randomDigitNotNull,
+                'job_id' => $faker->randomElement($jobIds),
+                'department_id' => $faker->randomElement($departmentIds)
+            ]);
+
+        User::create([
+                'first_name' => 'Vladimir',
+                'last_name' => 'Cherniuk',
+                'email' => 'reegerye@gmail.com',
+                'phone' => $faker->phoneNumber,
+                'avatar' => 'http://www.gravatar.com/avatar/' . $faker->md5. '?d=identicon',
+                'address' => $faker->address,
+                'password' => bcrypt('password'),
+                'reputation' => $faker->randomDigitNotNull,
+                'job_id' => $faker->randomElement($jobIds),
+                'department_id' => $faker->randomElement($departmentIds)
+            ]);
+
+        User::create([
+                'first_name' => 'Alexey',
+                'last_name' => 'Vdovichenko',
+                'email' => 'alexey.vdovichenko@binary-studio.com',
+                'phone' => $faker->phoneNumber,
+                'avatar' => 'http://www.gravatar.com/avatar/' . $faker->md5. '?d=identicon',
+                'address' => $faker->address,
+                'password' => bcrypt('password'),
+                'reputation' => $faker->randomDigitNotNull,
+                'job_id' => $faker->randomElement($jobIds),
+                'department_id' => $faker->randomElement($departmentIds)
+            ]);
+
+
+        User::create([
+                'first_name' => 'Michael',
+                'last_name' => 'Morozov',
+                'email' => 'michael.morozov@binary-studio.com',
+                'phone' => $faker->phoneNumber,
+                'avatar' => 'http://www.gravatar.com/avatar/' . $faker->md5. '?d=identicon',
+                'address' => $faker->address,
+                'password' => bcrypt('password'),
+                'reputation' => $faker->randomDigitNotNull,
+                'job_id' => $faker->randomElement($jobIds),
+                'department_id' => $faker->randomElement($departmentIds)
+            ]);
+
         foreach(range(1,10) as $index)
         {
             User::create([
