37bed0af5db5dbc4219ccf69683a7b921aa3184f
a1ex7 <artstroy14@mail.ru>
2015-08-21 23:06:27 +0300

    fix: add seed, 3 tag in every RR

--- database/seeds/TagTableSeeder.php
+++ database/seeds/TagTableSeeder.php
@@ -14,11 +14,34 @@ class TagTableSeeder extends Seeder
     {
         $faker = Faker\Factory::create();
 
-        foreach(range(1,20) as $index)
-        {
+        foreach (range(1, 20) as $index) {
             Tag::create([
                 'title' => $faker->unique()->word,
             ]);
         }
+
+        // Insert 3 random tag in every RR
+
+        $tagIds    = Tag::lists('id')->toArray();
+        $reviewIds = \App\ReviewRequest::lists('id')->toArray();
+
+        foreach ($reviewIds as $rid) {
+            DB::table('tag_review_request')->insert([
+                [
+                    'review_request_id' => $rid,
+                    'tag_id'            => $faker->randomElement($tagIds),
+                ],
+                [
+                    'review_request_id' => $rid,
+                    'tag_id'            => $faker->randomElement($tagIds),
+                ],
+                [
+                    'review_request_id' => $rid,
+                    'tag_id'            => $faker->randomElement($tagIds),
+                ]
+            ]);
+        }
+
+
     }
 }
