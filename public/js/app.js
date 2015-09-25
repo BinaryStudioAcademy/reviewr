2a8e0302ea768c396a0cc51b9f72696a286f5497
a1ex7 <artstroy14@mail.ru>
2015-09-25 10:43:20 +0300

    change web_socket connection to 5588

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,7 @@
 
         prefix: window.APP_PREFIX,
         apiPrefix: '/api/v1',
+        websocketPort: 5588,
 
         getPrefix: function () {
             return this.prefix + this.apiPrefix
