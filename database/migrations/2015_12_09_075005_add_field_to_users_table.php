786e09b50962f27c76a4cfc376761157555ae078
antarus66 <andriy.tarusin@binary-studio.com>
2015-12-09 08:12:42 +0000

    user-id: Added fields to the users table

---
+++ database/migrations/2015_12_09_075005_add_field_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('binary_id', 24)->nullable()->unique();
+            $table->string('thumb_avatar')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('binary_id');
+            $table->dropColumn('thumb_avatar');
+        });
+    }
+}
