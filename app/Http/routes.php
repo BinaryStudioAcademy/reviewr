5bb4b4f6666701db14d5c91ce34559d2035fa811
antarus66 <andriy.tarusin@binary-studio.com>
2015-12-11 17:45:08 +0200

    code-refactoring: Update routes style

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -26,43 +26,165 @@ Route::group (['prefix' => env('APP_PREFIX', '')], function () {
     Route::get('/auth/me/{binary_id}', 'Mockups\AuthController@profile');
 
     Route::group([ 'prefix' => 'api/v1' ], function () {
-        Route::get('reviewrequest/my', 'ReviewRequestController@myReviewRequest');
-        Route::get('reviewrequest/offered', 'ReviewRequestController@offeredReviewRequest');
-        Route::get('myrequests', 'UserController@myRequests');
-        Route::get('reviewrequest/offered_', 'ReviewRequestController@offeredReviewRequests');
-        Route::get('reviewrequest/popular', 'ReviewRequestController@popularReviewRequests');
-        Route::get('reviewrequest/last/{number}', 'ReviewRequestController@lastNReviewRequests');
-        Route::get('reviewrequest/upcoming/{period}', 'ReviewRequestController@upcomingForPeriodReviewRequests');
-        Route::get('reviewrequest/upcoming', 'ReviewRequestController@upcomingReviewRequests');
-        Route::get('reviewrequest/high_rate', 'ReviewRequestController@highestRatedReviewRequests');
-        Route::get('reviewrequest/group/{group_id}', 'ReviewRequestController@sortReviewRequestsByGroups');
-        Route::get('reviewrequest/tag/{tag_id}', 'ReviewRequestController@sortReviewRequestsByTags');
-        Route::get('reviewrequest/user/{user_id}', 'ReviewRequestController@sortReviewRequestsByUsers');
-        Route::get('reviewrequest/{request_id}/checkvote', 'ReviewRequestController@checkVote');
-        Route::get('reputationUp/{request_id}', 'ReviewRequestController@reputationUp');
-        Route::get('reputationDown/{request_id}', 'ReviewRequestController@reputationDown');
-        Route::get('usersforrequest/{request_id}', 'ReviewRequestController@usersForRequest');
-        Route::resource('unreadnotifications', 'UserController@unreadNotifications');
-        Route::get('users/high_rep', 'UserController@highRept');
-        Route::get('reviewrequest/heigh_rep/{number}', 'ReviewRequestController@getHeighRept');
-        Route::resource('user', 'UserController', ['only' => ['index', 'show']]);
-        Route::resource('/reviewrequest/{rid}/comment', 'CommentController', ['only' => ['index', 'store', 'show']]);
-        Route::resource('job', 'JobController', ['only' => ['index']]);
-        Route::resource('department', 'DepartmentController', ['only' => ['index']]);
-        Route::resource('reviewrequest', 'ReviewRequestController', ['except' => ['create', 'edit']]);
-        Route::get('reviewrequest/{id}/offers', 'ReviewRequestController@offers');
-        Route::get('reviewrequest/{id}/tags', 'ReviewRequestController@tags');
-        Route::get('user/{user_id}/accept/{request_id}', 'UserController@acceptReviewRequest');
-        Route::get('user/{user_id}/decline/{request_id}', 'UserController@declineReviewRequest');
-        Route::get('user/{user_id}/offeron/{request_id}', 'UserController@offerOnReviewRequest');
-        Route::get('user/offeroff/{request_id}', 'UserController@offerOffReviewRequest');
-        Route::get('user/{user_id}/mailaccept/{request_id}', 'UserController@mailAcceptReviewRequest');
-        Route::get('user/{user_id}/maildecline/{request_id}', 'UserController@mailDeclineReviewRequest');
-        Route::resource('group', 'GroupController', ['only' => ['index']]);
-        Route::get('tags/popular', 'TagController@popularTags');
-        Route::resource('tag', 'TagController', ['only' => ['index']]);
-        Route::resource('badge', 'BadgeController', ['only' => ['index']]);
-        Route::post('tags/search', 'TagController@search');
-        Route::get('checknotification', 'UserController@checkNotification');
+        Route::get(
+            'reviewrequest/my',
+            'ReviewRequestController@myReviewRequest'
+        );
+        Route::get(
+            'reviewrequest/offered',
+            'ReviewRequestController@offeredReviewRequest'
+        );
+        Route::get(
+            'myrequests',
+            'UserController@myRequests'
+        );
+        Route::get(
+            'reviewrequest/offered_',
+            'ReviewRequestController@offeredReviewRequests'
+        );
+        Route::get(
+            'reviewrequest/popular',
+            'ReviewRequestController@popularReviewRequests'
+        );
+        Route::get(
+            'reviewrequest/last/{number}',
+            'ReviewRequestController@lastNReviewRequests'
+        );
+        Route::get(
+            'reviewrequest/upcoming/{period}',
+            'ReviewRequestController@upcomingForPeriodReviewRequests'
+        );
+        Route::get(
+            'reviewrequest/upcoming',
+            'ReviewRequestController@upcomingReviewRequests'
+        );
+        Route::get(
+            'reviewrequest/high_rate',
+            'ReviewRequestController@highestRatedReviewRequests'
+        );
+        Route::get(
+            'reviewrequest/group/{group_id}',
+            'ReviewRequestController@sortReviewRequestsByGroups'
+        );
+        Route::get(
+            'reviewrequest/tag/{tag_id}',
+            'ReviewRequestController@sortReviewRequestsByTags'
+        );
+        Route::get(
+            'reviewrequest/user/{user_id}',
+            'ReviewRequestController@sortReviewRequestsByUsers'
+        );
+        Route::get(
+            'reviewrequest/{request_id}/checkvote',
+            'ReviewRequestController@checkVote'
+        );
+        Route::get(
+            'reputationUp/{request_id}',
+            'ReviewRequestController@reputationUp'
+        );
+        Route::get(
+            'reputationDown/{request_id}',
+            'ReviewRequestController@reputationDown'
+        );
+        Route::get(
+            'usersforrequest/{request_id}',
+            'ReviewRequestController@usersForRequest'
+        );
+        Route::resource(
+            'unreadnotifications',
+            'UserController@unreadNotifications'
+        );
+        Route::get(
+            'users/high_rep',
+            'UserController@highRept'
+        );
+        Route::get(
+            'reviewrequest/heigh_rep/{number}',
+            'ReviewRequestController@getHeighRept'
+        );
+        Route::resource(
+            'user',
+            'UserController',
+            ['only' => ['index', 'show']]
+        );
+        Route::resource(
+            '/reviewrequest/{rid}/comment',
+            'CommentController',
+            ['only' => ['index', 'store', 'show']]
+        );
+        Route::resource(
+            'job',
+            'JobController',
+            ['only' => ['index']]
+        );
+        Route::resource(
+            'department',
+            'DepartmentController',
+            ['only' => ['index']]
+        );
+        Route::resource(
+            'reviewrequest',
+            'ReviewRequestController',
+            ['except' => ['create', 'edit']]
+        );
+        Route::get(
+            'reviewrequest/{id}/offers',
+            'ReviewRequestController@offers'
+        );
+        Route::get(
+            'reviewrequest/{id}/tags',
+            'ReviewRequestController@tags'
+        );
+        Route::get(
+            'user/{user_id}/accept/{request_id}',
+            'UserController@acceptReviewRequest'
+        );
+        Route::get(
+            'user/{user_id}/decline/{request_id}',
+            'UserController@declineReviewRequest'
+        );
+        Route::get(
+            'user/{user_id}/offeron/{request_id}',
+            'UserController@offerOnReviewRequest'
+        );
+        Route::get(
+            'user/offeroff/{request_id}',
+            'UserController@offerOffReviewRequest'
+        );
+        Route::get(
+            'user/{user_id}/mailaccept/{request_id}',
+            'UserController@mailAcceptReviewRequest'
+        );
+        Route::get(
+            'user/{user_id}/maildecline/{request_id}',
+            'UserController@mailDeclineReviewRequest'
+        );
+        Route::resource(
+            'group',
+            'GroupController',
+            ['only' => ['index']]
+        );
+        Route::get(
+            'tags/popular',
+            'TagController@popularTags'
+        );
+        Route::resource(
+            'tag',
+            'TagController',
+            ['only' => ['index']]
+        );
+        Route::resource(
+            'badge',
+            'BadgeController',
+            ['only' => ['index']]
+        );
+        Route::post(
+            'tags/search',
+            'TagController@search'
+        );
+        Route::get(
+            'checknotification',
+            'UserController@checkNotification'
+        );
     });
 });
